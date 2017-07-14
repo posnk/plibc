958b4a1fdecad6e3128ecdea134a6f81f8dd65c7
peterbjornx <peterbosc@gmail.com>
2017-07-14 19:09:38 +0100

    Implement memmove

---
+++ src/string/memmove.c
@@ -0,0 +1,54 @@
+/******************************************************************************\
+Copyright (C) 2017 Peter Bosch
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+\******************************************************************************/
+
+/**
+ * @file string/memmove.c
+ *
+ * Part of plibc
+ *
+ * Written by Peter Bosch <peterbosc@gmail.com>
+ *
+ */
+#include <plcconfig.h>
+#include <string.h>
+
+/** memmove - copy bytes in memory */
+void *		memmove	(      void *dest, const void *src,        size_t n)
+{	
+	const char *_s;
+	char *_d;
+	size_t p;
+	
+	//TODO: Make this work on non-flat memory models
+	//TODO: Verify the behaviour for systems where sizeof(char) != 1 byte
+	_s = src;
+	_d = dest;
+	
+	if ( dest == src ) {
+		return dest;
+	} else if ( dest < src ) {
+		for ( p = 0; p < n; p++ )
+			_d[ p ] = _s[ p ];
+	} else {
+		for ( p = n; p > 0; p-- )
+			_d[ p - 1 ] = _s[ p - 1 ];
+	}
+		
+	return dest;
+
+}
